2ada9c859dba6cac51e7336b9a4429903d303462
Md. Rafiul Islam <rislamshaon95@gmail.com>
2023-08-30 05:49:00 +0600

    added show details modal

--- phone.js
+++ phone.js
@@ -1,23 +1,25 @@
-const loadPhone = async (searchText) => {
+const loadPhone = async (searchText, isShowAll) => {
     const res = await fetch (`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones = data.data;
-    displayPhones(phones);
+    displayPhones(phones, isShowAll);
 }
 
-const displayPhones = (phones) =>{
+const displayPhones = (phones, isShowAll) =>{
     const phoneContainer = document.getElementById('phone-container');
     phoneContainer.innerText = '';
 
     const showAllContainer = document.getElementById('show-all-container');
 
-    if(phones.length > 12){
+    if(phones.length > 12 && !isShowAll){
       showAllContainer.classList.remove('hidden');
     } else {
       showAllContainer.classList.add('hidden');
     }
 
-    phones = phones.slice(0, 12);
+    if(!isShowAll){
+      phones = phones.slice(0, 12);
+    }
     
     phones.forEach(phone => {
         console.log(phone)
@@ -30,7 +32,7 @@ const displayPhones = (phones) =>{
           <h2 class="card-title">${phone.phone_name}</h2>
           <p>If a dog chews shoes whose shoes does he choose?</p>
           <div class="card-actions justify-center">
-            <button class="btn btn-primary">Buy Now</button>
+            <button onclick="showDetails('${phone.slug}')" class="btn btn-primary">Show Details</button>
           </div>
         </div>
         `;
@@ -39,11 +41,40 @@ const displayPhones = (phones) =>{
     toggleSpinner(false);
 }
 
-const handleSearch = () =>{
+const showDetails = async(id) => {
+   const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+   const data = await res.json();
+   const phone = data.data;
+   showPhoneDetails(phone);
+}
+
+const showPhoneDetails = (phone) =>{
+
+  const showDetailContainer = document.getElementById('show-details-container');
+
+  showDetailContainer.innerHTML = `
+      <img src="${phone.image}" >
+      <h3 class="font-bold text-3xl">${phone.name}</h3>
+      <p><span class="font-bold">Storage: </span>${phone?.mainFeatures?.storage}</p>
+      <p><span class="font-bold">Display Size: </span>${phone?.mainFeatures?.displaySize}</p>
+      <p><span class="font-bold">Chipset: </span>${phone?.mainFeatures?.chipSet}</p>
+      <p><span class="font-bold">Memory: </span>${phone?.mainFeatures?.memory}</p>
+      <p><span class="font-bold">Slug: </span>${phone?.slug}</p>
+      <p><span class="font-bold">Released Date: </span>${phone?.releaseDate}</p>
+      <p><span class="font-bold">Brand: </span>${phone?.brand}</p>
+      <p><span class="font-bold">GPS: </span>${phone?.others?.GPS || 'No GPS available'}</p>
+  `
+
+  show_details_modal.showModal();
+}
+
+
+
+const handleSearch = (isShowAll) =>{
   toggleSpinner(true);
    const searchField = document.getElementById('search-field');
    const searchText = searchField.value;
-   loadPhone(searchText);
+   loadPhone(searchText, isShowAll);
 }
 
 const toggleSpinner = (isLoading) => {
@@ -53,4 +84,8 @@ const toggleSpinner = (isLoading) => {
   } else {
     loadingSpinner.classList.add('hidden');
   }
+}
+
+const showAllBtn = () =>{
+    handleSearch(true);
 }
